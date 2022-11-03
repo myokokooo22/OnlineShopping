b781ebe3016ef3d43e1cc199ce46db4c20a72c88
unknown <myokokooo911@gmail.com>
2022-11-04 01:49:43 +0630

    01:47 04 Nov 2022

---
+++ js/app.js
@@ -0,0 +1,57 @@
+let addbtns = document.querySelectorAll('.addtocart');
+let lists = document.querySelector('.lists');
+const itemLists = [];
+
+addbtns.forEach((addbtn)=>{
+    addbtn.addEventListener('click',(e)=>{
+       let price = e.target.parentNode.previousElementSibling;
+       let name = e.target.parentNode.parentNode.childNodes[1];
+       let imgs = e.target.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].src;
+      
+        let cloneItemName = name.cloneNode(true);
+        let clonePrice = price.cloneNode(true);
+        // let cloneimgs = imgs.cloneNode(true)
+
+       let createDiv = document.createElement('div');
+       createDiv.classList.add('lists');
+    //    console.log(createDiv)
+       createDiv.innerHTML = `
+       <div class="row">
+       <div class="col-4">
+          
+       </div>
+       <div class="col-4">
+           <h5>${cloneItemName.innerText}</h5>
+       </div>
+       <div class="col-4">
+           <span>$</span><span class="itemprice">${(clonePrice.innerText)}</span>
+       </div>
+   </div>
+ </div>
+       `;
+    // lists.appendChild(cloneItemName);
+    // lists.appendChild(clonePrice);
+    // lists.appendChild(cloneimgs);
+
+    lists.appendChild(createDiv);
+    itemLists.push(createDiv);
+
+
+    let pricedata = [];
+    let priceClone = lists.querySelectorAll('.itemprice');
+    priceClone.forEach((pcl)=>{
+        let innerPrice = Number(pcl.innerText);
+        // console.log(innerPrice)
+        pricedata.push(innerPrice);
+
+        let finalPrice = pricedata.reduce((prev,next)=>{
+            return prev + next;
+        },0);
+
+        // console.log(finalPrice)
+
+        let updateprice = document.getElementById('total');
+        updateprice.textContent = finalPrice;
+    })
+    });
+})
